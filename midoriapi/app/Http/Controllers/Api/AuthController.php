0b009ec89785c8f442e8890406b8d8b97c7b7f0f
shinichikudo45 <shinichikudo045697@gmail.com>
2025-12-03 08:34:47 +0700

    03 Desember 2025 Lab Pemrograman

---
+++ midoriapi/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register (Request $request) {
+        $validator = Validator::make($request -> all(),[
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8'
+
+        ]);
+
+        if($validator->fails()){
+            return response() -> json ($validator -> error());
+        }
+
+        $user = User::create([
+            'name' => $request -> name,
+            'email' => $request -> email,
+            'password' => Hash::make($request -> password)
+        ]);
+
+        $token = $user -> createToken('auth_token') -> plainTextToken;
+    }
+};
